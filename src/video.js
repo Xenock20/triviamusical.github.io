1eb59c60898bde2b00e46f65c3758c10d938f7ab
Xenock20 <juliosaboredo@gmail.com>
2022-09-09 00:51:09 -0300

    Titulo e imagen agregada

--- src/video.js
+++ src/video.js
@@ -9,8 +9,8 @@
       var player;
       function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
-          height: '360',
-          width: '640',
+          height: '300',
+          width: '600',
           videoId: 'rQ2KLveYGDU',
           playerVars: {
             autoPlay: 1,
