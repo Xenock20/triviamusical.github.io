8644918f7f1432996ad7eb4ed6787262dcb27693
Xenock20 <juliosaboredo@gmail.com>
2022-09-16 14:49:18 -0300

    Juego Final v1.0

--- src/main.js
+++ src/main.js
@@ -8,12 +8,12 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 let player1
 let player2
-//let player3
+let player3
 function onYouTubeIframeAPIReady() {
   player1 = new YT.Player('playerOne', {
     height: '200',
     width: '400',
-    videoId: videosId[videoWin].id, //cargar video atravez de la id
+    videoId: videosId[videoWin1].id, //cargar video atravez de la id
     playerVars: {
       autoPlay: 1,
       controls: 0,
@@ -24,7 +24,7 @@ function onYouTubeIframeAPIReady() {
   player2 = new YT.Player('playerTwo', {
     height: '200',
     width: '400',
-    videoId: videosId[0].id, //cargar video atravez de la id
+    videoId: videosId[videoWin2].id, //cargar video atravez de la id
     playerVars: {
       autoPlay: 1,
       controls: 0,
@@ -32,17 +32,17 @@ function onYouTubeIframeAPIReady() {
     },
   });
 
-  /*
+  
   player3 = new YT.Player('playerThre', {
     height: '200',
     width: '400',
-    videoId: videosId[1].id, //cargar video atravez de la id
+    videoId: videosId[videoWin3].id, //cargar video atravez de la id
     playerVars: {
       autoPlay: 1,
       controls: 0,
       disablekb: 0
     },
-  });*/
+  });
 }
 
 //Fin de la api de youtube
@@ -54,6 +54,8 @@ const video3 = document.getElementById('vl3')
 const game = document.getElementById('trivia')
 const home = document.getElementById('cont-play')
 const play = document.getElementById('play')
+const reset = document.getElementById('r')
+
 const lv1 = document.getElementById('lv1')
 let point1 = 100
 lv1.innerText = point1
@@ -63,42 +65,47 @@ lv2.innerText = point2
 const lv3 = document.getElementById('lv3')
 let point3 = 100
 lv3.innerText = point3
+
+const puntos = document.getElementById('puntos')
 const soundPlay = new Audio('./sound/play.mp3')
 const soundWin = new Audio('./sound/victory.mp3')
 const soundError = new Audio('./sound/error.mp3')
-const videoWin = Math.floor(Math.random() * 15)
+
+const videoWin1 = Math.floor(Math.random() * 15)
+const videoWin2 = Math.floor(Math.random() * 15)
+const videoWin3 = Math.floor(Math.random() * 15)
+
 const numRandomBoton1 = Math.floor(Math.random() * 4)
 const numRandomBoton2 = Math.floor(Math.random() * 4)
-//const numRandomBoton3 = Math.floor(Math.random() * 4)
-const botonWin1 = document.getElementById(`option${numRandomBoton1}`)
-const botonWin2 = document.getElementById(`option${numRandomBoton2}`)
-//const botonWin3 = document.getElementById(`option${numRandomBoton3}`)
-console.log(botonWin1)
-console.log(botonWin2)
-//console.log(botonWin3)
+const numRandomBoton3 = Math.floor(Math.random() * 4)
+let botonWin
+let botonDefear
+let botonDefear2
+let botonDefear3
+let botonDefear4
 
 const next = document.getElementById('next')
 
 const arrTitle = []
 
-while(arrTitle.length < 100){
+while(arrTitle.length < 150){
 
   let n = Math.floor(Math.random() * 15)
 
-  if(n !== videoWin){
+  if(n !== videoWin1 && n !== videoWin2 && n !== videoWin3){
     arrTitle.push(n)
   }
 }
 
 const newArrTitle = [...new Set(arrTitle)]
 
-const arr = [0,1,2,3,4]
-const newArr = arr.filter(ele => ele !== numRandomBoton1)
+console.log(newArrTitle)
 
-const botonDefear = document.getElementById(`option${newArr[0]}`)
-const botonDefear2 = document.getElementById(`option${newArr[1]}`)
-const botonDefear3 = document.getElementById(`option${newArr[2]}`)
-const botonDefear4 = document.getElementById(`option${newArr[3]}`)
+const arr = [0,1,2,3,4]
+const newArrLv1 = arr.filter(ele => ele !== numRandomBoton1)
+const newArrLv2 = arr.filter(ele => ele !== numRandomBoton2)
+const newArrLv3 = arr.filter(ele => ele !== numRandomBoton3)
+
 const botones = document.querySelectorAll('.op')
 
 const videosId = [
@@ -138,14 +145,13 @@ function botonW(e){
   e.target.style.background = 'var(--verde)'
   e.target.style.border = '5px solid var(--verde-borde)'
   e.target.style.color = 'white'
-  video1.style.filter = 'none'
-  lv1.style.backgroundColor = 'var(--verde)'
 
   notclicked()
 
   level++
 
   next.style.display = 'block'
+
 }
 
 function notclicked(){
@@ -157,6 +163,9 @@ function notclicked(){
 function clicked(){
   botones.forEach(ele => {
     ele.style.pointerEvents = 'auto'
+    ele.style.background = 'var(--violeta-oscuro)'
+    ele.style.border = '5px solid var(--violeta-oscuro-borde)'
+    ele.style.color = 'var(--azul-oscuro-background)'
   });
 }
 
@@ -165,25 +174,23 @@ function botonD(e){
   e.target.style.background = 'var(--rojo)'
   e.target.style.border = '5px solid var(--rojo-borde)'
   e.target.style.color = 'white'
-  point1 -= 20
-  lv1.innerHTML = point1
+  e.target.style.pointerEvents = 'none'
+  
+  if(level === 1){
+    point1 -= 20
+    lv1.innerHTML = point1
+  }
+
+  if(level === 2){
+    point2 -= 20
+    lv2.innerHTML = point2
+  }
+
+  if(level === 3){
+    point3 -= 20
+    lv3.innerHTML = point3
+  }
 }
-
-botonDefear.addEventListener('click', (e) =>{
-  botonD(e)
-})
-
-botonDefear2.addEventListener('click', (e) =>{
-  botonD(e)
-})
-
-botonDefear3.addEventListener('click', (e) =>{
-  botonD(e)
-})
-
-botonDefear4.addEventListener('click', (e) =>{
-  botonD(e)
-})
 
 next.addEventListener('click', (e) =>{
   if(level === 2){
@@ -191,10 +198,19 @@ next.addEventListener('click', (e) =>{
     next.style.display = 'none'
   }
   
-  /*if(level === 3){
+  if(level === 3){
     plv3()
     next.style.display = 'none'
-  }*/
+  }
+
+  if(level > 3){
+    player3.stopVideo()
+    game.style.display = 'none'
+
+    puntos.innerText = String((point1 + point2 + point3))
+
+    document.querySelector('.point').style.display = 'grid'
+  }
 
   clicked()
 })
@@ -202,64 +218,171 @@ next.addEventListener('click', (e) =>{
 function plv1(){
   player1.playVideo()
 
-  botonWin1.addEventListener('click', (e) =>{
-    botonW(e)
+  botonWin = document.getElementById(`option${numRandomBoton1}`)
+
+  botonWin.addEventListener('click', (e) =>{
+    if(level === 1){
+      botonW(e)
+      video1.style.filter = 'none'
+      lv1.style.backgroundColor = 'var(--verde)'
+    }
   })
 
-  botonWin1.innerText = player1.videoTitle
+  botonDefear = document.getElementById(`option${newArrLv1[0]}`)
+  botonDefear2 = document.getElementById(`option${newArrLv1[1]}`)
+  botonDefear3 = document.getElementById(`option${newArrLv1[2]}`)
+  botonDefear4 = document.getElementById(`option${newArrLv1[3]}`)
+
+  botonWin.innerText = player1.videoTitle
   botonDefear.innerText = videosId[newArrTitle[0]].title
   botonDefear2.innerText = videosId[newArrTitle[1]].title
   botonDefear3.innerText = videosId[newArrTitle[2]].title
   botonDefear4.innerText = videosId[newArrTitle[3]].title
+
+  botonDefear.addEventListener('click', (e) =>{
+    if(level === 1){
+      botonD(e)
+    }
+  })
+  
+  botonDefear2.addEventListener('click', (e) =>{
+    if(level === 1){
+      botonD(e)
+    }
+  })
+  
+  botonDefear3.addEventListener('click', (e) =>{
+    if(level === 1){
+      botonD(e)
+    }
+  })
+  
+  botonDefear4.addEventListener('click', (e) =>{
+    if(level === 1){
+      botonD(e)
+    }
+  })
 }
+
 
 function plv2(){
   player1.stopVideo();
+  player2.playVideo()
 
   lv1.style.border = 'solid 2px var(--violeta-oscuro-borde)'
   lv2.style.border = 'solid 2px white';
   video1.style.display = 'none'
   video2.style.display = 'block'
 
-  botonWin2.addEventListener('click', (e) =>{
-    botonW(e)
+  botonWin = document.getElementById(`option${numRandomBoton2}`)
+
+  botonWin.addEventListener('click', (e) =>{
+    if(level === 2){
+      botonW(e)
+      video2.style.filter = 'none'
+      lv2.style.backgroundColor = 'var(--verde)'
+    }
   })
 
-  botonWin2.innerText = player2.videoTitle
+  botonDefear = document.getElementById(`option${newArrLv2[0]}`)
+  botonDefear2 = document.getElementById(`option${newArrLv2[1]}`)
+  botonDefear3 = document.getElementById(`option${newArrLv2[2]}`)
+  botonDefear4 = document.getElementById(`option${newArrLv2[3]}`)
+
+  botonWin.innerText = player2.videoTitle
   botonDefear.innerText = videosId[newArrTitle[4]].title
   botonDefear2.innerText = videosId[newArrTitle[5]].title
   botonDefear3.innerText = videosId[newArrTitle[6]].title
   botonDefear4.innerText = videosId[newArrTitle[7]].title
+
+  botonDefear.addEventListener('click', (e) =>{
+    if(level === 2){
+      botonD(e)
+    }
+  })
+  
+  botonDefear2.addEventListener('click', (e) =>{
+    if(level === 2){
+      botonD(e)
+    }
+  })
+  
+  botonDefear3.addEventListener('click', (e) =>{
+    if(level === 2){
+      botonD(e)
+    }
+  })
+  
+  botonDefear4.addEventListener('click', (e) =>{
+    if(level === 2){
+      botonD(e)
+    }
+  })
 }
 
-/*
+
 function plv3(){
   player2.stopVideo();
+  player3.playVideo()
 
   lv2.style.border = 'solid 2px var(--violeta-oscuro-borde)'
   lv3.style.border = 'solid 2px white';
   video2.style.display = 'none'
   video3.style.display = 'block'
 
-  botonWin3.addEventListener('click', (e) =>{
-    botonW(e)
+  botonWin = document.getElementById(`option${numRandomBoton3}`)
+
+  botonWin.addEventListener('click', (e) =>{
+    if(level === 3){
+      botonW(e)
+      video3.style.filter = 'none'
+      lv3.style.backgroundColor = 'var(--verde)'
+    }
   })
 
-  botonWin3.innerText = player3.videoTitle
+  botonDefear = document.getElementById(`option${newArrLv3[0]}`)
+  botonDefear2 = document.getElementById(`option${newArrLv3[1]}`)
+  botonDefear3 = document.getElementById(`option${newArrLv3[2]}`)
+  botonDefear4 = document.getElementById(`option${newArrLv3[3]}`)
+
+  botonWin.innerText = player3.videoTitle
   botonDefear.innerText = videosId[newArrTitle[8]].title
   botonDefear2.innerText = videosId[newArrTitle[9]].title
   botonDefear3.innerText = videosId[newArrTitle[10]].title
-  botonDefear4.innerText = videosId[newArrTitle1[1]].title
-}
-*/
+  botonDefear4.innerText = videosId[newArrTitle[11]].title
 
+  botonDefear.addEventListener('click', (e) =>{
+    if(level === 3){
+      botonD(e)
+    }
+  })
+  
+  botonDefear2.addEventListener('click', (e) =>{
+    if(level === 3){
+      botonD(e)
+    }
+  })
+  
+  botonDefear3.addEventListener('click', (e) =>{
+    if(level === 3){
+      botonD(e)
+    }
+  })
+  
+  botonDefear4.addEventListener('click', (e) =>{
+    if(level === 3){
+      botonD(e)
+    }
+  })
+}
+
+reset.addEventListener('click', () => {
+  location.reload();
+})
 
 //TRABAJO POR REALIZAR
 
 /*
-*Arreglar los titulos
 *Agregar cambio alateorio para los videos
 *Desactivar boton ya apretado
-
-*Agregar tercer nivel
 */
